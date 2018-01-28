a10e4947f6b85f371df5198f2778cbcc59acb5fe
gengxin1995 <525038358@qq.com>
2018-01-28 13:25:58 +0800

    add houghCircles

---
+++ src/Chapter6/houghCircles.cpp
@@ -0,0 +1,28 @@
+//
+// Created by 耿欣 on 2018/1/28.
+//
+
+#include <cv.h>
+#include <highgui.h>
+#include <math.h>
+
+int main(int argc, char *argv[]) {
+    IplImage *image = cvLoadImage(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+
+    CvMemStorage *storage = cvCreateMemStorage(0);
+    cvSmooth(image, image, CV_GAUSSIAN, 5, 5);
+
+    CvSeq *results = cvHoughCircles(image, storage, CV_HOUGH_GRADIENT, 2, image->width / 10);
+
+    for (int i = 0; i < results->total; i++) {
+        float *p = (float *) cvGetSeqElem(results, i);
+        CvPoint pt = cvPoint(cvRound(p[0]), cvRound(p[1]));
+        cvCircle(image, pt, cvRound(p[2]), CV_RGB(255, 255, 255));
+    }
+
+    cvNamedWindow("cvHoughCircles", 1);
+    cvShowImage("cvHoughCircles", image);
+    cvWaitKey(0);
+    cvReleaseImage(&image);
+    return 0;
+}
